c04d0fe1321c66e059795711eed01594ae75d8bd
vinisvp <viniciuspedro0905@gmail.com>
2024-03-22 15:21:07 -0300

    first a

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Student } from '../student';
+import { FormBuilder , FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-alunos',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AlunosComponent {
 
-}
+  students: Student[] = [];
+  studentFormGroup : FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+this.studentFormGroup =formBuilder.group({
+  id:['sasasa'],
+  name:['sasasa'],
+  course:['sasasa'],
+});
+  }
+
+  save(){
+    alert('click');
+  }
+  }
+
+  ;
+
